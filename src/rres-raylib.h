e7b12438bdf9df7a6d3891865476c28ff87e251c
Ray <raysan5@gmail.com>
2022-05-03 13:04:55 +0200

    ADDED: AES-CTR-256 encryption

--- src/rres-raylib.h
+++ src/rres-raylib.h
@@ -132,6 +132,7 @@ static int UnpackDataFromResourceChunk(rresResourceChunk *chunk);   // Unpack co
                                                                     // NOTE: Function return 0 on success or other value on failure
 
 static const char *GetFourCCFromType(unsigned int type)             // Get FourCC 4-char code from resource type, useful for log info
+static unsigned int *ComputeMD5(unsigned char *data, int size);     // Compute MD5 hash code, returns 4 integers array (static)
 
 //----------------------------------------------------------------------------------
 // Module Functions Definition
@@ -511,16 +512,76 @@ static int UnpackDataFromResourceChunk(rresResourceChunk *chunk)
     // STEP 1. Decrypt message if encrypted
     if (chunk->cipherType != RRES_CIPHER_NONE)
     {
-        if (chunk->cipherType == RRES_CIPHER_XCHACHA20_POLY1305)    // rrespacker supported encryption
+        if (chunk->cipherType == RRES_CIPHER_AES)   // rrespacker implements AES-CTR 256 bit
+        {
+#if defined(RRES_SUPPORT_ENCRYPTION_AES)
+            // WARNING: Implementation dependant! 
+            // rrespacker tool appends (salt + MD5) to encrypted data for convenience,
+            // Actually, chunk->packedSize considers those additional elements
+            
+            // Get some memory for the possible message output
+            unsigned char *decryptedData = (unsigned char *)RL_CALLOC(chunk->packedSize - 16 - 16, 1);
+            if (decryptedData != NULL) memcpy(decryptedData, chunk->data, chunk->packedSize - 16 - 16);
+
+            // Required variables for key stretching
+            uint8_t key[32] = { 0 };                    // Encryption key
+            const uint32_t blocks = 16384;              // Key stretching blocks: 16 megabytes
+            const uint32_t iterations = 3;              // Key stretching iterations: 3 iterations
+            void *workArea = RL_MALLOC(blocks*1024);    // Key stretching work area
+            uint8_t salt[16] = { 0 };                   // Key stretching salt
+            
+            // Retrieve salt from chunk packed data
+            // salt is stored at the end of packed data, before nonce and MAC: salt[16] + MD5[16]
+            memcpy(salt, ((unsigned char *)chunk->data) + (chunk->packedSize - 16 - 16), 16);
+
+            // Encryption key, generated from user password, using Argon2i algorythm for key stretching (256 bit)
+            crypto_argon2i(key, 32, workArea, blocks, iterations, (uint8_t *)rresGetCipherPassword(), 16, salt, 16);
+
+            // Wipe key generation secrets, they are no longer needed
+            crypto_wipe(salt, 16);
+            RL_FREE(workArea);
+            
+            // Required variables for decryption and message authentication
+            unsigned int md5[4] = { 0 };                // Message Authentication Code generated on encryption
+            
+            // Retrieve MD5 from chunk packed data
+            // NOTE: MD5 is stored at the end of packed data, after salt: salt[16] + MD5[16]
+            memcpy(md5, ((unsigned char *)chunk->data) + (chunk->packedSize - 16), 4*sizeof(unsigned int));
+
+            // Message decryption, requires key
+            struct AES_ctx ctx = { 0 };
+            AES_init_ctx(&ctx, key);
+            AES_CTR_xcrypt_buffer(&ctx, (uint8_t *)decryptedData, info.packedSize - 16 - 16);   // AES Counter mode, stream cipher
+            
+            // Verify MD5 to check if data decryption worked
+            unsigned int decryptMD5[4] = { 0 };
+            unsigned int *md5Ptr = ComputeMD5(decryptedData, info.packedSize - 16 - 16);
+            for (int i = 0; i < 4; i++) decryptMD5[i] = md5Ptr[i];
+
+            // Wipe secrets if they are no longer needed
+            crypto_wipe(key, 32);
+
+            if (memcmp(decryptMD5, md5, 4*sizeof(unsigned int)) == 0)    // Decrypted successfully!
+            {
+                RRES_FREE(chunk->data);
+                chunk->data = decryptedData;
+                chunk->packedSize -= (16 + 16);    // We remove additional data size from packed size
+            }
+            else result = 2;    // Data was not decrypted as expected, wrong password or message corrupted
+#else
+            result = 1;         // Decryption algorithm not supported
+#endif  
+        }
+        if (chunk->cipherType == RRES_CIPHER_XCHACHA20_POLY1305)    // rrespacker implements XChaCha20-Poly1305
         {
 #if defined(RRES_SUPPORT_ENCRYPTION_MONOCYPHER)
-            // Get some memory for the possible message output
-            unsigned char *decryptedData = (unsigned char *)RL_CALLOC(chunk->packedSize, 1);
-            
             // WARNING: Implementation dependant! 
             // rrespacker tool appends (salt + nonce + MAC) to encrypted data for convenience,
             // Actually, chunk->packedSize considers those additional elements
 
+            // Get some memory for the possible message output
+            unsigned char *decryptedData = (unsigned char *)RL_CALLOC(chunk->packedSize - 16 - 24 - 16, 1);
+            
             // Required variables for key stretching
             uint8_t key[32] = { 0 };                    // Encryption key
             const uint32_t blocks = 16384;              // Key stretching blocks: 16 megabytes
@@ -541,7 +602,7 @@ static int UnpackDataFromResourceChunk(rresResourceChunk *chunk)
             
             // Required variables for decryption and message authentication
             uint8_t nonce[24] = { 0 };                  // nonce used on encryption, unique to processed file
-            uint8_t mac[16] = { 0 };                    // Message Authentication Code generated on ecryption
+            uint8_t mac[16] = { 0 };                    // Message Authentication Code generated on encryption
             
             // Retrieve nonce and MAC from chunk packed data
             // nonce and MAC are stored at the end of packed data, after salt: salt[16] + nonce[24] + MAC[16]
@@ -654,6 +715,122 @@ static const char *GetFourCCFromType(unsigned int type)
         case RRES_DATA_DIRECTORY: return "CDIR";    // Central directory for input files relation to resource chunks
         default: break;
     }
+}
+
+// Compute MD5 hash code, returns 4 integers array (static)
+static unsigned int *ComputeMD5(unsigned char *data, int size)
+{
+#define LEFTROTATE(x, c) (((x) << (c)) | ((x) >> (32 - (c))))
+
+    static unsigned int hash[4] = { 0 };
+
+    // NOTE: All variables are unsigned 32 bit and wrap modulo 2^32 when calculating
+
+    // r specifies the per-round shift amounts
+    unsigned int r[] = {
+        7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+        5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
+        4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+        6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
+    };
+
+    // Use binary integer part of the sines of integers (in radians) as constants// Initialize variables:
+    unsigned int k[] = {
+        0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+        0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+        0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+        0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+        0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+        0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+        0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+        0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+        0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+        0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+        0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+        0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+        0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+        0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+        0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+        0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+    };
+
+    hash[0] = 0x67452301;
+    hash[1] = 0xefcdab89;
+    hash[2] = 0x98badcfe;
+    hash[3] = 0x10325476;
+
+    // Pre-processing: adding a single 1 bit
+    // Append '1' bit to message
+    // NOTE: The input bytes are considered as bits strings,
+    // where the first bit is the most significant bit of the byte
+
+    // Pre-processing: padding with zeros
+    // Append '0' bit until message length in bit 448 (mod 512)
+    // Append length mod (2 pow 64) to message
+
+    int newDataSize = ((((size + 8)/64) + 1)*64) - 8;
+
+    unsigned char *msg = RL_CALLOC(newDataSize + 64, 1);   // Also appends "0" bits (we alloc also 64 extra bytes...)
+    memcpy(msg, data, size);
+    msg[size] = 128;                 // Write the "1" bit
+
+    unsigned int bitsLen = 8*size;
+    memcpy(msg + newDataSize, &bitsLen, 4);  // We append the len in bits at the end of the buffer
+
+    // Process the message in successive 512-bit chunks for each 512-bit chunk of message
+    for (int offset = 0; offset < newDataSize; offset += (512/8))
+    {
+        // Break chunk into sixteen 32-bit words w[j], 0 <= j <= 15
+        unsigned int *w = (unsigned int *)(msg + offset);
+
+        // Initialize hash value for this chunk
+        unsigned int a = hash[0];
+        unsigned int b = hash[1];
+        unsigned int c = hash[2];
+        unsigned int d = hash[3];
+
+        for (int i = 0; i < 64; i++)
+        {
+            unsigned int f, g;
+
+            if (i < 16)
+            {
+                f = (b & c) | ((~b) & d);
+                g = i;
+            }
+            else if (i < 32)
+            {
+                f = (d & b) | ((~d) & c);
+                g = (5*i + 1)%16;
+            }
+            else if (i < 48)
+            {
+                f = b ^ c ^ d;
+                g = (3*i + 5)%16;
+            }
+            else
+            {
+                f = c ^ (b | (~d));
+                g = (7*i)%16;
+            }
+
+            unsigned int temp = d;
+            d = c;
+            c = b;
+            b = b + LEFTROTATE((a + f + k[i] + w[g]), r[i]);
+            a = temp;
+        }
+
+        // Add chunk's hash to result so far
+        hash[0] += a;
+        hash[1] += b;
+        hash[2] += c;
+        hash[3] += d;
+    }
+
+    RL_FREE(msg);
+
+    return hash;
 }
 
 #endif // RRES_RAYLIB_IMPLEMENTATION
