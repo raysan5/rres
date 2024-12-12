49b7673c064df9af1ad0d1cd4763726c3fc0b662
Ray <raysan5@gmail.com>
2024-12-12 13:27:10 +0100

    REMOVED: `ComputeMD5()`, function already available in raylib

--- src/rres-raylib.h
+++ src/rres-raylib.h
@@ -168,7 +168,6 @@ static char *LoadTextFromResourceChunk(rresResourceChunk chunk, unsigned int *co
 static Image LoadImageFromResourceChunk(rresResourceChunk chunk);                        // Load chunk: RRES_DATA_IMAGE
 
 static const char *GetExtensionFromProps(unsigned int ext01, unsigned int ext02);        // Get file extension from RRES_DATA_RAW properties (unsigned int) 
-static unsigned int *ComputeMD5(const unsigned char *data, int size);                    // Compute MD5 hash code, returns 4 integers array (static)
 
 //----------------------------------------------------------------------------------
 // Module Functions Definition
@@ -566,8 +565,8 @@ int UnpackResourceChunk(rresResourceChunk *chunk)
                 .nb_lanes  = 1                          // Single-threaded
             };
             crypto_argon2_inputs inputs = {
-                .pass = (const uint8_t *)rresGetCipherPassword(),   // User password
-                .pass_size = strlen(rresGetCipherPassword()),       // Password length
+                .pass = (const uint8_t *)rresGetCipherPassword(), // User password
+                .pass_size = (uint32_t)strlen(rresGetCipherPassword()), // Password length
                 .salt = salt,                           // Salt for the password
                 .salt_size = 16
             };
@@ -641,8 +640,8 @@ int UnpackResourceChunk(rresResourceChunk *chunk)
                 .nb_lanes  = 1                          // Single-threaded
             };
             crypto_argon2_inputs inputs = {
-                .pass = (const uint8_t *)rresGetCipherPassword(),   // User password
-                .pass_size = strlen(rresGetCipherPassword()),       // Password length
+                .pass = (const uint8_t *)rresGetCipherPassword(), // User password
+                .pass_size = (uint32_t)strlen(rresGetCipherPassword()), // Password length
                 .salt = salt,                           // Salt for the password
                 .salt_size = 16
             };
@@ -973,122 +972,6 @@ static const char *GetExtensionFromProps(unsigned int ext01, unsigned int ext02)
     extension[7] = (unsigned char)(ext02 & 0x000000ff);
 
     return extension;
-}
-
-// Compute MD5 hash code, returns 4 integers array (static)
-static unsigned int *ComputeMD5(const unsigned char *data, int size)
-{
-#define LEFTROTATE(x, c) (((x) << (c)) | ((x) >> (32 - (c))))
-
-    static unsigned int hash[4] = { 0 };
-
-    // NOTE: All variables are unsigned 32 bit and wrap modulo 2^32 when calculating
-
-    // r specifies the per-round shift amounts
-    unsigned int r[] = {
-        7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
-        5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
-        4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
-        6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
-    };
-
-    // Use binary integer part of the sines of integers (in radians) as constants// Initialize variables:
-    unsigned int k[] = {
-        0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
-        0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-        0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
-        0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-        0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
-        0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
-        0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
-        0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
-        0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
-        0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
-        0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
-        0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
-        0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
-        0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
-        0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
-        0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
-    };
-
-    hash[0] = 0x67452301;
-    hash[1] = 0xefcdab89;
-    hash[2] = 0x98badcfe;
-    hash[3] = 0x10325476;
-
-    // Pre-processing: adding a single 1 bit
-    // Append '1' bit to message
-    // NOTE: The input bytes are considered as bits strings,
-    // where the first bit is the most significant bit of the byte
-
-    // Pre-processing: padding with zeros
-    // Append '0' bit until message length in bit 448 (mod 512)
-    // Append length mod (2 pow 64) to message
-
-    int newDataSize = ((((size + 8)/64) + 1)*64) - 8;
-
-    unsigned char *msg = (unsigned char *)RL_CALLOC(newDataSize + 64, 1);   // Also appends "0" bits (we alloc also 64 extra bytes...)
-    memcpy(msg, data, size);
-    msg[size] = 128;                 // Write the "1" bit
-
-    unsigned int bitsLen = 8*size;
-    memcpy(msg + newDataSize, &bitsLen, 4);  // We append the len in bits at the end of the buffer
-
-    // Process the message in successive 512-bit chunks for each 512-bit chunk of message
-    for (int offset = 0; offset < newDataSize; offset += (512/8))
-    {
-        // Break chunk into sixteen 32-bit words w[j], 0 <= j <= 15
-        unsigned int *w = (unsigned int *)(msg + offset);
-
-        // Initialize hash value for this chunk
-        unsigned int a = hash[0];
-        unsigned int b = hash[1];
-        unsigned int c = hash[2];
-        unsigned int d = hash[3];
-
-        for (int i = 0; i < 64; i++)
-        {
-            unsigned int f, g;
-
-            if (i < 16)
-            {
-                f = (b & c) | ((~b) & d);
-                g = i;
-            }
-            else if (i < 32)
-            {
-                f = (d & b) | ((~d) & c);
-                g = (5*i + 1)%16;
-            }
-            else if (i < 48)
-            {
-                f = b ^ c ^ d;
-                g = (3*i + 5)%16;
-            }
-            else
-            {
-                f = c ^ (b | (~d));
-                g = (7*i)%16;
-            }
-
-            unsigned int temp = d;
-            d = c;
-            c = b;
-            b = b + LEFTROTATE((a + f + k[i] + w[g]), r[i]);
-            a = temp;
-        }
-
-        // Add chunk's hash to result so far
-        hash[0] += a;
-        hash[1] += b;
-        hash[2] += c;
-        hash[3] += d;
-    }
-
-    RL_FREE(msg);
-
-    return hash;
 }
 
 #endif // RRES_RAYLIB_IMPLEMENTATION
